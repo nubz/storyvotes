96dce2c45932afc12f7f00a68e8311048e4501c8
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2020-05-02 17:04:09 +0100

    test working but depends on firebase connection

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+import './index.css';
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+test('renders story votes link', () => {
+  const { getByText } = render(
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>)
+  const linkElement = getByText(/StoryVotes/i);
   expect(linkElement).toBeInTheDocument();
 });
